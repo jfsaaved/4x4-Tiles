4f54ab53a559723d0b1bbceada5d45d49c36360d
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2015-07-02 15:07:22 -0400

    Android Game - 4x4 Tiles
    -Highscore On Screen
    -Changed package name (for google play)

--- core/src/com/juliansaavedra/tiles/states/MenuState.java
+++ core/src/com/juliansaavedra/tiles/states/MenuState.java
@@ -15,19 +15,27 @@ public class MenuState extends State {
     //private TextImage title;
     private TextImage settings;
     private TextImage exit;
+    private TextImage highScoreImage;
 
     private Texture background;
     private Texture title;
     private int currentBGX = 1920;
     private float timer = 1;
 
+    private int highScore;
+
+
+
     public MenuState(GSM gsm) {
         super(gsm);
+
+        highScore = MainGame.pref.getHighScore();
 
         //title = new TextImage(MainGame.TITLE.toUpperCase(), MainGame.WIDTH / 2, MainGame.HEIGHT / 2 + 200 , 1);
         play = new TextImage("START", MainGame.WIDTH / 2, MainGame.HEIGHT / 2, 1);
         settings = new TextImage("SETTINGS", MainGame.WIDTH / 2, MainGame.HEIGHT / 2 - 100, 1);
         exit = new TextImage("EXIT", MainGame.WIDTH / 2, MainGame.HEIGHT / 2 - 200, 1);
+        highScoreImage = new TextImage("HIGHSCORE "+highScore, MainGame.WIDTH/2 + 150, MainGame.HEIGHT/2 - 300, 0.50f);
 
         background = MainGame.res.getTexture("bg");
         title = MainGame.res.getTexture("title");
@@ -78,6 +86,7 @@ public class MenuState extends State {
         play.render(sb);
         settings.render(sb);
         exit.render(sb);
+        highScoreImage.render(sb);
 
         sb.draw(title,MainGame.WIDTH/2 - 150,MainGame.HEIGHT/2 + 100);
         sb.end();
