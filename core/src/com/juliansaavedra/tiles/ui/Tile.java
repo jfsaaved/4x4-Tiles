e204b211537b680817287fbd1f9a0b32d380f582
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2015-08-02 15:49:04 -0400

    Android Game - 4x4 Tiles
    Removed cdTimer, and changed cdToggle

--- core/src/com/juliansaavedra/tiles/ui/Tile.java
+++ core/src/com/juliansaavedra/tiles/ui/Tile.java
@@ -10,6 +10,7 @@ import com.juliansaavedra.tiles.MainGame;
  */
 public class Tile extends Box {
 
+
     private TextureRegion light;
     private TextureRegion dark;
     private TextureRegion wrong;
